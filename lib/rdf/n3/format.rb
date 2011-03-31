ee2a4716b662273206f93704d04f54015f209d63
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-03-30 17:29:50 -0700

    Yardoc update.

--- lib/rdf/n3/format.rb
+++ lib/rdf/n3/format.rb
@@ -2,24 +2,24 @@ module RDF::N3
   ##
   # RDFa format specification.
   #
-  # @example Obtaining an RDFa format class
-  #   RDF::Format.for(:n3)            #=> RDF::N3::Format
-  #   RDF::Format.for("etc/foaf.ttl")
-  #   RDF::Format.for("etc/foaf.n3")
-  #   RDF::Format.for(:file_name      => "etc/foaf.ttl")
-  #   RDF::Format.for(:file_name      => "etc/foaf.n3")
-  #   RDF::Format.for(:file_extension => "ttl")
-  #   RDF::Format.for(:file_extension => "n3")
-  #   RDF::Format.for(:content_type   => "text/turtle")
-  #   RDF::Format.for(:content_type   => "text/n3")
+  # @example Obtaining an Notation3 format class
+  #     RDF::Format.for(:n3)            #=> RDF::N3::Format
+  #     RDF::Format.for("etc/foaf.ttl")
+  #     RDF::Format.for("etc/foaf.n3")
+  #     RDF::Format.for(:file_name      => "etc/foaf.ttl")
+  #     RDF::Format.for(:file_name      => "etc/foaf.n3")
+  #     RDF::Format.for(:file_extension => "ttl")
+  #     RDF::Format.for(:file_extension => "n3")
+  #     RDF::Format.for(:content_type   => "text/turtle")
+  #     RDF::Format.for(:content_type   => "text/n3")
   #
   # @example Obtaining serialization format MIME types
-  #   RDF::Format.content_types      #=> {"text/turtle" => [RDF::N3::Format]}
-  #   RDF::Format.content_types      #=> {"text/n3")" => [RDF::N3::Format]}
+  #     RDF::Format.content_types      #=> {"text/turtle" => [RDF::N3::Format]}
+  #     RDF::Format.content_types      #=> {"text/n3")" => [RDF::N3::Format]}
   #
   # @example Obtaining serialization format file extension mappings
-  #   RDF::Format.file_extensions    #=> {:ttl => "text/turtle"}
-  #   RDF::Format.file_extensions    #=> {:n3 => "text/n3"}
+  #     RDF::Format.file_extensions    #=> {:ttl => "text/turtle"}
+  #     RDF::Format.file_extensions    #=> {:n3 => "text/n3"}
   #
   # @see http://www.w3.org/TR/rdf-testcases/#ntriples
   class Format < RDF::Format
@@ -39,10 +39,10 @@ module RDF::N3
   #
   # This allows the following:
   #
-  # @example Obtaining an RDFXML format class
-  #   RDF::Format.for(:ttl)         # RDF::N3::Notation3
-  #   RDF::Format.for(:ttl).reader  # RDF::N3::Reader
-  #   RDF::Format.for(:ttl).writer  # RDF::N3::Writer
+  # @example Obtaining an Notation3 format class
+  #     RDF::Format.for(:ttl)         #=> RDF::N3::Notation3
+  #     RDF::Format.for(:ttl).reader  #=> RDF::N3::Reader
+  #     RDF::Format.for(:ttl).writer  #=> RDF::N3::Writer
   class Notation3 < RDF::Format
     reader { RDF::N3::Reader }
     writer { RDF::N3::Writer }
@@ -52,10 +52,10 @@ module RDF::N3
   #
   # This allows the following:
   #
-  # @example Obtaining an RDFXML format class
-  #   RDF::Format.for(:ttl)         # RDF::N3::TTL
-  #   RDF::Format.for(:ttl).reader  # RDF::N3::Reader
-  #   RDF::Format.for(:ttl).writer  # RDF::N3::Writer
+  # @example Obtaining an TTL format class
+  #     RDF::Format.for(:ttl)         # RDF::N3::TTL
+  #     RDF::Format.for(:ttl).reader  # RDF::N3::Reader
+  #     RDF::Format.for(:ttl).writer  # RDF::N3::Writer
   class TTL < RDF::Format
     reader { RDF::N3::Reader }
     writer { RDF::N3::Writer }
@@ -65,10 +65,10 @@ module RDF::N3
   #
   # This allows the following:
   #
-  # @example Obtaining an RDFXML format class
-  #   RDF::Format.for(:turtle)         # RDF::N3::Turtle
-  #   RDF::Format.for(:turtle).reader  # RDF::N3::Reader
-  #   RDF::Format.for(:turtle).writer  # RDF::N3::Writer
+  # @example Obtaining an Turtle format class
+  #     RDF::Format.for(:turtle)         # RDF::N3::Turtle
+  #     RDF::Format.for(:turtle).reader  # RDF::N3::Reader
+  #     RDF::Format.for(:turtle).writer  # RDF::N3::Writer
   class Turtle < RDF::Format
     reader { RDF::N3::Reader }
     writer { RDF::N3::Writer }
