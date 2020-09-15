20618aaf422bf46a1e0097b2aece411b879da30c
Gregg Kellogg <gregg@greggkellogg.net>
2020-09-15 15:07:15 -0700

    log:conjunction creates a formula by combining the operands of the constituent formulae of the subject list.

--- lib/rdf/n3/algebra/log/conjunction.rb
+++ lib/rdf/n3/algebra/log/conjunction.rb
@@ -3,11 +3,32 @@ module RDF::N3::Algebra::Log
   # A function to merge formulae: logical AND.
   #
   # The subject is a list of formulae. The object, which can be generated, is a formula containing a copy of each of the formulae in the list on the left. A cwm built-in function.
-  class Conjunction < SPARQL::Algebra::Operator::Binary
-    include SPARQL::Algebra::Query
-    include SPARQL::Algebra::Update
+  class Conjunction < RDF::N3::Algebra::ListOperator
     include RDF::N3::Algebra::Builtin
 
     NAME = :logConjunction
+
+    ##
+    # Evaluates this operator by creating a new formula containing the triples from each of the formulae in the list.
+    #
+    # @param [RDF::N3::List] list
+    # @return [RDF::N3::Algebra::Formula]
+    # @see RDF::N3::ListOperator#evaluate
+    def evaluate(list)
+      form = RDF::N3::Algebra::Formula.new() 
+      
+      list.each do |f|
+        form.operands.append(*f.operands)
+      end
+      form
+    end
+
+    ##
+    # Return subject operand.
+    #
+    # @return [RDF::Term]
+    def input_operand
+      operands.first
+    end
   end
 end
